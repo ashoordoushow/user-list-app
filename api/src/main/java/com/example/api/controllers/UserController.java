010455b26a59d1ae0f86d11db43ff99d08317cbf
ashoordoushow <ashoor12323@gmail.com>
2025-11-03 17:52:24 -0600

    Add enabled CORS for React frontend on port 5173

--- api/src/main/java/com/example/api/controllers/UserController.java
+++ api/src/main/java/com/example/api/controllers/UserController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@CrossOrigin(origins = "http://localhost:*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserRepository userRepository;
